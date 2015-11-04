284ac0944a8b236808a353816376abb8149bb3e5
Jacob <jacoblisboa@gmail.com>
2015-11-04 13:08:02 -0300

    alterando la
    la

--- functions.php
+++ functions.php
@@ -1,3 +1,5 @@
 <!--  MENU  ----------------------------------------------- -->
-	<?php require_once(bloginfo('template_directory').'/wp_bootstrap_navwalker.php'); ?>
+	<?php 
+	teste
+	require_once(bloginfo('template_directory').'/wp_bootstrap_navwalker.php'); ?>
 		<?php register_nav_menus( array('primary' => __( 'Primary Menu', 'mentor' ), ) ); ?>
